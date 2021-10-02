809c02a0477426827e5fb1c08ef0866706565a5d
Unicorn-Soo <91789032+Unicorn-Soo@users.noreply.github.com>
2021-10-02 22:13:19 +0800

    Delete layout72 directory

--- layout72/student.js
+++
@@ -1,7 +0,0 @@
-function topLinkClicked(obj) {
-    let otherNavs = document.getElementsByClassName("nav-link");
-    for (let i = 0; i < otherNavs.length; i++) {
-        otherNavs[i].classList.remove('active');
-    }
-    obj.classList.add('active');
-}
